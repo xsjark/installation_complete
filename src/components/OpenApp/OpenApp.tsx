3806708f2ca673ace9397d082a4cde531a4b9966
Ridha Watanabe <xsjark@protonmail.com>
2022-12-21 17:10:37 -0500

    opens the runfood app

--- src/components/OpenApp/OpenApp.tsx
+++ src/components/OpenApp/OpenApp.tsx
@@ -3,9 +3,13 @@ import { Box } from "@mui/system";
 
 export default function OpenApp() {
     return (
-        <Box sx={{py: 1}}>
+        <Box sx={{ py: 1 }}>
             <Typography variant="h6" align="center">RunFood está listo para usarse.</Typography>
-            <Typography variant="h6" align="center"><Link>Abre la App</Link></Typography>
+            <Typography variant="h6" align="center">
+                <Link href="runfood://open?svr=localhost&port=1000">
+                    Abre la App
+                </Link>
+            </Typography>
         </Box>
     )
 }
